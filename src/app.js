2a5aff79f76242055f85d4e28d50aaa02fcb03d5
Rafael Gomes <rafarfl2010@gmail.com>
2020-06-01 16:36:15 -0300

    creat initial funtions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log('live');
+const cadForm = document.getElementById('cadForm');
+
+cadForm.addEventListener('submit', function(e) {
+    
+    if (cadForm.checkValidity()) {
+        console.info('formulario preenchido');
+    } else {
+        console.error('erro');
+    }
+}, false);
+
+storageSetup = () => {
+    if(!localStorage.db) localStorage.setItem('db', "localStorage");
+
+    return localStorage.getItem('db');
+}
+
+autoLOad = () => {
+    dbSelected = document.getElementById('dbSelected');
+
+    dbSelected.innerHTML = `Você está usando: ${storageSetup()}`;
+}
