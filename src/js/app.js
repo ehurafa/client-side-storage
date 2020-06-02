8b0c52af3420db8bdaf3f614fc69d540476b3326
Rafael Gomes <rafarfl2010@gmail.com>
2020-06-02 17:51:17 -0300

    filter in localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,146 @@
 
- getAll(localStorage.getItem('db'));
+
+const table = document.getElementById('contacts').getElementsByTagName('tbody')[0];
+
+const iniDb = (type) => {  console.log('func  iniDb ', type);
+   if (type == "localStorage") {
+        if (!localStorage.getItem("contacts"))  {
+            localStorage.setItem("contacts", JSON.stringify([]));  
+        }                 
+   }
+}
+
+const cleanDb = () => {
+    
+    if (localStorage.getItem("db"))  {
+        localStorage.removeItem("db");
+    }
+       
+}
+
+const save = (type, contact) => { 
+
+    iniDb(type);
+
+    if (type == "localStorage") {
+        let contacts = JSON.parse(localStorage.getItem("contacts"));
+
+        contacts.push(contact);
+        localStorage.setItem("contacts", JSON.stringify(contacts));
+
+        let item = [];
+
+        item.push(contact);
+
+        renderLine(item);
+    }  
+} 
+
+const getAll = (type) => {
+    iniDb(type);   
+
+    if (type == "localStorage") {
+        const contacts = JSON.parse(localStorage.getItem("contacts"));
+
+       // renderAll(contacts);
+    }
+}
+
+const renderLine = (contact) => {  
+
+    factoryContact(table, contact);
+
+} 
+
+const search = (type, term) => {  console.log('search ', term);
+    iniDb(type);
+
+    console.log('type ', type);
+
+    if (type == "localStorage") {  console.log('1 ');
+        const contacts = JSON.parse(localStorage.getItem("contacts"));
+
+        const contactsFound = [];
+
+        contacts.forEach((contact) => { console.log('2 ');
+            console.log(contact);
+            if (contact.name == term || contact.email == term) {
+                contactsFound.push(contact);
+            }
+            console.log(contactsFound);
+        });
+        renderAll(contactsFound);
+    }
+}  
+
+const factoryContact = (table, contact) => { // console.log('factoryContact ', contact);
+    
+    contact.forEach((item, i, array) => {    
+            
+        const newRow = table.insertRow(table.rows.length);
+
+        const cellName = newRow.insertCell(0);
+
+        const cellEmail = newRow.insertCell(1);
+
+        const cellPhone = newRow.insertCell(2);
+
+        const cellButton = newRow.insertCell(3);
+
+        const name = document.createTextNode(item['name']);
+
+        const email = document.createTextNode(item['email']);
+
+        const phone = document.createTextNode(item['phone']);
+
+        cellName.appendChild(name);
+        cellEmail.appendChild(email);
+        cellPhone.appendChild(phone);
+
+        cellButton.innerHTML = "<button type='button' onclick='removeContact(this)'>x</button>"; 
+
+       // console.log(newRow);
+
+  }); 
+} 
+
+
+const renderAll = (contacts) => {   console.log('renderAll');
+
+let  table = document.getElementById('contacts').getElementsByTagName('tbody')[0];
+
+    const newTbody = document.createElement('tbody');    
+
+    table.parentNode.replaceChild(newTbody, table);
+
+     table = document.getElementById('contacts').getElementsByTagName('tbody')[0];
+
+   console.log('table  ', table );
+
+   console.log('renderAll contacts ', contacts);
+
+   factoryContact(table, contacts);
+      
+}
+
+// renderAll();
+
+// renderLine();
+
+/////////
+
+ // getAll(localStorage.getItem('db'));
 
 const cadForm = document.getElementById('cadForm');
 
 cadForm.addEventListener('submit', function(e) {
+
+    e.preventDefault();
     
     console.log(e);
     if (cadForm.checkValidity()) {
 
-        const formData = new formData(cadForm);
+        const formData = new FormData(cadForm);
 
         const contact = {
             'name': formData.get('contact.name'),
@@ -16,7 +148,7 @@ cadForm.addEventListener('submit', function(e) {
             'phone': formData.get('contact.phone')
         }
 
-        save(localStorage.db, contact);
+        save("localStorage", contact);
 
         cadForm.reset();
 
@@ -28,18 +160,29 @@ cadForm.addEventListener('submit', function(e) {
     }
 }, false);
 
-const selectecDb = (type) => {
+const autoLoad = () => {
+    dbSelected = document.getElementById('dbSelected');
+
+    dbSelected.innerHTML = `Você está usando: ${storageSetup()}`;
+}
+
+const selectedDb = (type) => {  
+  
     cleanDb();
-    localStorage.setItem('db', type);
-    autoload();
-    console.log('Tipo selecionado ', localStorage.getItem('db'));
+    localStorage.setItem('db', type.options[type.selectedIndex].value);
+    autoLoad();
+
 }
 
 searchEvt = document.getElementById('search');
 
 searchEvt.addEventListener('click', (evt) => {
 
-    const term = document.getElementBy('filter').value;
+    
+
+    const term = document.getElementById('filter').value;
+
+    console.log(term);
 
     search(storageSetup(), term);
 });
@@ -59,8 +202,3 @@ storageSetup = () => {
     return localStorage.getItem('db');
 }
 
-autoLOad = () => {
-    dbSelected = document.getElementById('dbSelected');
-
-    dbSelected.innerHTML = `Você está usando: ${storageSetup()}`;
-}
